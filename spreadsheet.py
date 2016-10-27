11607daa94020ee020b7e31d563ebdbb624f99d5
Cyclicduck <caleb.ji@wustl.edu>
2016-10-27 15:08:27 -0500

    using googleapiclients, adding extras to try to make it run

--- spreadsheet.py
+++ spreadsheet.py
@@ -2,14 +2,14 @@ from __future__ import print_function
 import httplib2
 import os
 
-from apiclient import discovery
+from googleapiclient import discovery
 from oauth2client import client
 from oauth2client import tools
 from oauth2client.file import Storage
 
 try:
     import argparse
-    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+    flags, extras = argparse.ArgumentParser(parents=[tools.argparser]).parse_args(None)
 except ImportError:
     flags = None
 
